9cd35664dfce5ec5f57e56154e0fc3ac09132d8d
u1975103 <99675316+u1975103@users.noreply.github.com>
2022-02-28 17:41:01 +0100

    Adding nav_p2 button

---
+++ pi_2/js/menu_controller.js
@@ -0,0 +1,22 @@
+function start_game(){
+	name = prompt("User name");
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+}
+
+function options(){
+	// TODO: Open options menu
+	console.log("Options menu button");
+}
+
+function nav_p2(){
+	loadpage("./pi_2/index.html")
+}
+
+
